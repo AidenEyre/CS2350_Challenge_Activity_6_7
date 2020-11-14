6c0d3922f15e8118df7db963af708abc7442c313
AidenEyre <EyreAiden@gmail.com>
2020-11-13 20:39:31 -0700

    Initialized starter code for this assignment.

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/*
+
+   6-7: Challenge: Handling Events on Form Elements
+   
+   Author: Aiden Eyre
+   Date:   13 November 2020
+   
+   Filename: index.js
+   
+   Function List
+   =============
+   
+   add functions here
+   
+*/
+
