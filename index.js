8762537340a56589b311025e3c311cfd21f05a3b
AidenEyre <EyreAiden@gmail.com>
2020-11-13 21:00:01 -0700

    Created a function to handle the pet weight and kennel size inputs.
    also created a window onload function.

--- index.js
+++ index.js
@@ -16,3 +16,28 @@
    
 */
 
+// When the page loads run these functions
+window.addEventListener("load", () => {
+    // Run this element everytime input changes
+    document.getElementById("weight").oninput = checkKennelSize;
+});
+
+// Function handles the pet weight and kennel size inputs
+function checkKennelSize() {
+    // Initialize petWeight and kennelSize nodes
+    var petWeight = document.getElementById("weight").value;
+    var kennelSize = document.getElementById("size");
+
+    // Run through a series of if statements to display a value in kennelSize
+    if(petWeight === NaN) {
+        kennelSize.value = "";
+    } else if (petWeight <= 4) {
+        kennelSize.value = "mini";
+    } else if (petWeight > 4 && petWeight <= 12) {
+        kennelSize.value = "small";
+    } else if (petWeight > 12 && petWeight <= 50) {
+        kennelSize.value = "medium";
+    } else if (petWeight > 50) {
+        kennelSize.value = "large";
+    }
+}
