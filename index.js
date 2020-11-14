06880de4281b871e691d0f1a55a5f67eaed61613
AidenEyre <EyreAiden@gmail.com>
2020-11-14 09:42:26 -0700

    Formatted document with "prettier" extension.

--- index.js
+++ index.js
@@ -32,117 +32,119 @@
 let style = document.createElement("style");
 document.head.appendChild(style);
 
-
 // When the page loads run these functions
 window.addEventListener("load", () => {
-    // Set the divs in the checkbox class to display none
-    style.sheet.insertRule("#singAdd{display:none}", 0);
-    style.sheet.insertRule("#cuteAdd{display:none}", 1);
-    style.sheet.insertRule("#trickAdd{display:none}", 2);
-    daysBoarding(); // Run this function on page load
+  // Set the divs in the checkbox class to display none
+  style.sheet.insertRule("#singAdd{display:none}", 0);
+  style.sheet.insertRule("#cuteAdd{display:none}", 1);
+  style.sheet.insertRule("#trickAdd{display:none}", 2);
+  daysBoarding(); // Run this function on page load
 
-    // Run these elements everytime input changes
-    document.getElementById("weight").oninput = checkKennelSize;
-    document.getElementById("days").oninput = daysBoarding;
-    // Use an arrow function to call two different functions in the onchange event
-    document.getElementById("sing").onchange = () => {
-        makeVisible("#sing", 0);
-        totalCost;
-    }
-    document.getElementById("cute").onchange = () => {
-        makeVisible("#cute", 1);
-        totalCost;
-    }
-    document.getElementById("trick").onchange = () => {
-        makeVisible("#trick", 2);
-        totalCost;
-    }
+  // Run these elements everytime input changes
+  document.getElementById("weight").oninput = checkKennelSize;
+  document.getElementById("days").oninput = daysBoarding;
+  // Use an arrow function to call two different functions in the onchange event
+  document.getElementById("sing").onchange = () => {
+    makeVisible("#sing", 0);
+    totalCost;
+  };
+  document.getElementById("cute").onchange = () => {
+    makeVisible("#cute", 1);
+    totalCost;
+  };
+  document.getElementById("trick").onchange = () => {
+    makeVisible("#trick", 2);
+    totalCost;
+  };
 });
-
 
 // Adds two decimal places after inputted letiables
 function formatNumber(val) {
-    // Call toLocaleString and have it give the number two decimal places
-    return val.toLocaleString(undefined, {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2,
-    });
+  // Call toLocaleString and have it give the number two decimal places
+  return val.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 }
 
 // Handles the pet weight and kennel size inputs
 function checkKennelSize() {
-    // Initialize petWeight and kennelSize nodes
-    let petWeight = document.getElementById("weight").value;
-    let kennelSize = document.getElementById("size");
+  // Initialize petWeight and kennelSize nodes
+  let petWeight = document.getElementById("weight").value;
+  let kennelSize = document.getElementById("size");
 
-    // Run through a series of if statements to display a value in kennelSize
-    if (petWeight === NaN) {
-        kennelSize.value = "";
-    } else if (petWeight <= 4) {
-        kennelSize.value = "mini";
-    } else if (petWeight > 4 && petWeight <= 12) {
-        kennelSize.value = "small";
-    } else if (petWeight > 12 && petWeight <= 50) {
-        kennelSize.value = "medium";
-    } else if (petWeight > 50) {
-        kennelSize.value = "large";
-    }
+  // Run through a series of if statements to display a value in kennelSize
+  if (petWeight === NaN) {
+    kennelSize.value = "";
+  } else if (petWeight <= 4) {
+    kennelSize.value = "mini";
+  } else if (petWeight > 4 && petWeight <= 12) {
+    kennelSize.value = "small";
+  } else if (petWeight > 12 && petWeight <= 50) {
+    kennelSize.value = "medium";
+  } else if (petWeight > 50) {
+    kennelSize.value = "large";
+  }
 }
 
 // Handles the days boarding and boardingFee inputs
 function daysBoarding() {
-    // Initialize days and boardingFee nodes
-    let days = document.getElementById("days").value;
-    let boardingFee = document.getElementById("boardingFee");
+  // Initialize days and boardingFee nodes
+  let days = document.getElementById("days").value;
+  let boardingFee = document.getElementById("boardingFee");
 
-    // If there is not input set days value to 0
-    if (days === "") {
-        days = "0";
-    }
+  // If there is not input set days value to 0
+  if (days === "") {
+    days = "0";
+  }
 
-    // Calculate the value by converting the days value to an int and
-    // multiplying by 19.99. Then finally calling the formatNumber()
-    // function
-    boardingFee.value = formatNumber(parseFloat(days) * 19.99);
+  // Calculate the value by converting the days value to an int and
+  // multiplying by 19.99. Then finally calling the formatNumber()
+  // function
+  boardingFee.value = formatNumber(parseFloat(days) * 19.99);
 
-    // Call totalCost() to update
-    totalCost();
+  // Call totalCost() to update
+  totalCost();
 }
 
 // Updates all the total costs at the bottom of the form
 function totalCost() {
-    // Initialize a few variables
-    let registerCost = 0;
-    let eventNum = 0;
-    let boardingCost = document.getElementById("boardingFee").value;
+  // Initialize a few variables
+  let registerCost = 0;
+  let eventNum = 0;
+  let boardingCost = document.getElementById("boardingFee").value;
 
-    // If ID: sing, cute, or trick is checked ad ome to eventNum
-    if (document.getElementById("sing").checked) {
-        eventNum++;
-    }
-    
-    if (document.getElementById("cute").checked) {
-        eventNum++;
-    }
-    
-    if (document.getElementById("trick").checked) {
-        eventNum++;
-    }
+  // If ID: sing, cute, or trick is checked ad ome to eventNum
+  if (document.getElementById("sing").checked) {
+    eventNum++;
+  }
 
-    // Calculate registraction cost
-    registerCost = eventNum*120;
+  if (document.getElementById("cute").checked) {
+    eventNum++;
+  }
 
-    // Update each input value
-    document.getElementById("boardingCost").value = formatNumber(boardingCost);
-    document.getElementById("registrationCost").value = formatNumber(registerCost);
-    document.getElementById("totalCost").value = formatNumber(parseFloat(registerCost) + parseFloat(boardingCost));
+  if (document.getElementById("trick").checked) {
+    eventNum++;
+  }
+
+  // Calculate registraction cost
+  registerCost = eventNum * 120;
+
+  // Update each input value
+  document.getElementById("boardingCost").value = formatNumber(boardingCost);
+  document.getElementById("registrationCost").value = formatNumber(
+    registerCost
+  );
+  document.getElementById("totalCost").value = formatNumber(
+    parseFloat(registerCost) + parseFloat(boardingCost)
+  );
 }
 
 // Changes the checkbox divs display from block to none
 function makeVisible(id, index) {
-    if(document.querySelector(id+":checked")) {
-        style.sheet.cssRules[index].style.display = "block";
-    } else {
-        style.sheet.cssRules[index].style.display = "none";
-    }
+  if (document.querySelector(id + ":checked")) {
+    style.sheet.cssRules[index].style.display = "block";
+  } else {
+    style.sheet.cssRules[index].style.display = "none";
+  }
 }
